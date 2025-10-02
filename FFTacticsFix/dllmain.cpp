5a44994938d5bd300b24b9e138f60c7d106ad087
trigger <triggerhappy187@live.com>
2025-10-02 00:20:31 -0700

    filter: fix viewport scaling when paused

--- FFTacticsFix/dllmain.cpp
+++ FFTacticsFix/dllmain.cpp
@@ -1,11 +1,9 @@
 #include <shlwapi.h>
 #include "Memory.h"
 #include "MinHook.h"
-#include <winternl.h>
-#include <intrin.h>
 
-HMODULE GameModule = GetModuleHandleA("FFT_enhanced.exe");
-uintptr_t GameBase = (uintptr_t)GameModule;
+HMODULE GameModule = 0;
+uintptr_t GameBase = 0;
 
 void InstallHooks()
 {
@@ -15,19 +13,30 @@ void InstallHooks()
 void ApplyPatches()
 {
     uintptr_t grainFilterOffset = (uintptr_t)Memory::PatternScan(GameModule, "38 ?? A0 55 02 00");
+    uintptr_t pauseMenuViewportResizeOffset = (uintptr_t)Memory::PatternScan(GameModule, "66 C7 80 ?? ?? ?? ?? 01 00");
 
-    if (!grainFilterOffset) {
-        return;
+    if (grainFilterOffset) {
+        DWORD oldProtect;
+        VirtualProtect((LPVOID)(grainFilterOffset), 6, PAGE_EXECUTE_READWRITE, &oldProtect);
+        *(uint32_t*)(grainFilterOffset + 0) = 0x90909090;
+        *(uint16_t*)(grainFilterOffset + 4) = 0x9090;
     }
 
-    DWORD oldProtect;
-    VirtualProtect((LPVOID)(grainFilterOffset), 6, PAGE_EXECUTE_READWRITE, &oldProtect);
-    *(uint32_t*)(grainFilterOffset + 0) = 0x90909090;
-    *(uint16_t*)(grainFilterOffset + 4) = 0x9090;
+    if (pauseMenuViewportResizeOffset) {
+        DWORD oldProtect;
+        VirtualProtect((LPVOID)(pauseMenuViewportResizeOffset), 9, PAGE_EXECUTE_READWRITE, &oldProtect);
+        *(uint32_t*)(pauseMenuViewportResizeOffset + 0) = 0x90909090;
+        *(uint32_t*)(pauseMenuViewportResizeOffset + 4) = 0x90909090;
+        *(uint16_t*)(pauseMenuViewportResizeOffset + 8) = 0x90;
+    }
+
 }
 
 DWORD WINAPI MainThread(LPVOID lpParam)
 {
+    GameModule = GetModuleHandleA("FFT_enhanced.exe");
+    GameBase = (uintptr_t)GameModule;
+
     if (GameModule == 0)
         return true;
 
